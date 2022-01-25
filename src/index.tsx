b10333158f91665cb2207c406a4f0bd9c89f3cf6
Tony Naughton <tonynaughton1@gmail.com>
2022-01-25 21:19:41 +0000

    Initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Typography from '@mui/material/Typography';
+import './index.css'
+import { createTheme } from '@mui/system';
+import { ThemeProvider } from '@emotion/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const customTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'Grandstander',
+      'Roboto Condensed'
+    ].join(',')
+  }
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = (): JSX.Element => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <div className="App">
+        <Typography variant='h3' component="h1" className="app-heading">
+          Welcome to FPL Zone!
+        </Typography>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render( <App/>, document.getElementById('root'));
