14e50b86e83a957e5fa932cc8873e564f2d83c35
Tony Naughton <tonynaughton1@gmail.com>
2022-02-08 18:57:43 +0000

    added further summary data, component container layout

--- src/components/gameweek_summary/gameweek_summary.tsx
+++ src/components/gameweek_summary/gameweek_summary.tsx
@@ -2,33 +2,43 @@ import React from "react";
 import { Gameweek } from "types/gameweek";
 import { Box, Grid, Typography } from "@mui/material";
 import { Player } from "types/player";
-import { Team } from "types/team";
+import { GetPlayerById } from "helpers";
 
 interface GameweekSummaryProps {
   gameweek?: Gameweek;
-  starPlayer?: Player;
-  topTeam?: Team;
+  players?: Player[];
 }
 
-export default function GameweekSummary({
-  gameweek,
-  starPlayer,
-  topTeam,
-}: GameweekSummaryProps): JSX.Element {
-  const gameweekStats = [
-    { label: "Star Player", data: `${starPlayer?.web_name} ${gameweek?.top_element_info.points}` },
-    { label: "Top Team", data: `${topTeam?.name} ${gameweek?.highest_score}` },
-    { label: "Average Score", data: gameweek?.average_entry_score },
+export default function GameweekSummary({ gameweek, players }: GameweekSummaryProps): JSX.Element {
+  const topPlayerId = gameweek?.top_element_info.id;
+  const starPlayer: Player | undefined = GetPlayerById(topPlayerId, players);
+  const mostTransferredIn: Player | undefined = GetPlayerById(
+    gameweek?.most_transferred_in,
+    players
+  );
+  const mostCaptained: Player | undefined = GetPlayerById(gameweek?.most_captained, players);
+  const mostViceCaptained: Player | undefined = GetPlayerById(
+    gameweek?.most_vice_captained,
+    players
+  );
+
+  const summaryData = [
+    { label: "star player:", data: `${starPlayer?.web_name}` },
+    { label: "highest score:", data: gameweek?.highest_score },
+    { label: "average score:", data: gameweek?.average_entry_score },
+    { label: "most transferred in:", data: mostTransferredIn?.web_name },
+    { label: "most captained:", data: mostCaptained?.web_name },
+    { label: "most vice-captained:", data: mostViceCaptained?.web_name },
   ];
 
   return (
     <Box sx={{ width: "100%" }}>
-      <Grid container alignItems="center" textAlign="center">
-        {gameweekStats.map((stat, index): JSX.Element => {
+      <Grid container alignItems="center" textAlign="center" rowGap={5} sx={{ mb: 5 }}>
+        {summaryData.map((stat, index): JSX.Element => {
           return (
             <Grid key={index} item xs={4}>
-              <Typography>{stat.label}</Typography>
-              <Typography>{stat.data}</Typography>
+              <Typography sx={{ fontSize: 25 }}>{stat.label.toUpperCase()}</Typography>
+              <Typography sx={{ fontSize: 25 }}>{stat.data}</Typography>
             </Grid>
           );
         })}
--- src/pages/gameweek_live_page.tsx
+++ src/pages/gameweek_live_page.tsx
@@ -3,14 +3,12 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "config/firebase";
 import AppLayout from "components/layout/app_layout";
-import { Box, Container } from "@mui/material";
 import GameweekSummary from "components/gameweek_summary/gameweek_summary";
 import { useQuery } from "react-query";
-import { getGameData, getTeamData } from "api/fpl_api_provider";
+import { getGameData } from "api/fpl_api_provider";
 import { Gameweek } from "types/gameweek";
 import { Player } from "types/player";
-import { GetPlayerById } from "helpers";
-import { Team } from "types/team";
+import ComponentContainer from "components/layout/component_container";
 
 export default function GameweekLivePage(): JSX.Element {
   const [user, loading] = useAuthState(auth);
@@ -24,33 +22,20 @@ export default function GameweekLivePage(): JSX.Element {
   const { data: gameData } = useQuery("game-data", getGameData);
 
   let gameweeks: Gameweek[];
-  let players: Player[];
+  let players: Player[] | undefined;
   let currentGameweek: Gameweek | undefined;
-  let starPlayer: Player | undefined;
 
   if (gameData) {
     gameweeks = gameData.events;
     players = gameData.elements;
     currentGameweek = gameweeks.find((gw) => gw.is_current) as Gameweek;
-    starPlayer = GetPlayerById(currentGameweek.top_element_info.id, players);
   }
-
-  const topTeamId = currentGameweek?.highest_scoring_entry;
-  const { data: topTeam } = useQuery<Team, Error>(
-    ["top-team", topTeamId],
-    () => getTeamData(topTeamId),
-    {
-      enabled: !!topTeamId,
-    }
-  );
 
   return (
     <AppLayout activeLabel="gameweek live">
-      <Container maxWidth="xl">
-        <Box width="100%" border="1px solid black">
-          <GameweekSummary gameweek={currentGameweek} starPlayer={starPlayer} topTeam={topTeam} />
-        </Box>
-      </Container>
+      <ComponentContainer title="summary">
+        <GameweekSummary gameweek={currentGameweek} players={players} />
+      </ComponentContainer>
     </AppLayout>
   );
 }
