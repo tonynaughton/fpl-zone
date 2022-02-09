e2c0c1926ab015cd108a1f3590d9c2ea011e8732
Tony Naughton <tonynaughton1@gmail.com>
2022-02-09 15:22:06 +0000

    Added crest images

--- src/components/gameweek_summary/gameweek_summary.tsx
+++ src/components/gameweek_summary/gameweek_summary.tsx
@@ -11,6 +11,12 @@ interface GameweekSummaryProps {
   players?: Player[];
 }
 
+interface SummaryDataItem {
+  label: string;
+  data?: string | number;
+  teamCode?: number;
+}
+
 export default function GameweekSummary({ gameweek, players }: GameweekSummaryProps): JSX.Element {
   const topPlayerId = gameweek?.top_element_info?.id;
   const starPlayer: Player | undefined = GetPlayerById(topPlayerId, players);
@@ -24,49 +30,54 @@ export default function GameweekSummary({ gameweek, players }: GameweekSummaryPr
     players
   );
 
-  const summaryData = [
+  const summaryData: SummaryDataItem[] = [
     {
       label: "star player:",
-      data: `${starPlayer?.web_name}` || "N/A",
+      data: `${starPlayer?.web_name}`,
       teamCode: starPlayer?.team_code,
     },
-    { label: "highest score:", data: gameweek?.highest_score || "N/A" },
-    { label: "average score:", data: gameweek?.average_entry_score || "N/A" },
+    { label: "highest score:", data: gameweek?.highest_score },
+    { label: "average score:", data: gameweek?.average_entry_score },
     {
       label: "most transferred in:",
-      data: mostTransferredIn?.web_name || "N/A",
+      data: mostTransferredIn?.web_name,
       teamCode: mostTransferredIn?.team_code,
     },
     {
       label: "most captained:",
-      data: mostCaptained?.web_name || "N/A",
+      data: mostCaptained?.web_name,
       teamCode: mostCaptained?.team_code,
     },
     {
       label: "most vice-captained:",
-      data: mostViceCaptained?.web_name || "N/A",
+      data: mostViceCaptained?.web_name,
       teamCode: mostViceCaptained?.team_code,
     },
   ];
 
+  const renderGridItem = (item: SummaryDataItem, key: number) => {
+    const img: JSX.Element | undefined = item.teamCode ? (
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/images/crests/${item.teamCode}.png`}
+        alt="crest-img"
+        key={key}
+        height={75}
+        width={75}
+      />
+    ) : undefined;
+    return (
+      <Grid item xs={4}>
+        {img}
+        <Typography sx={{ fontSize: 25 }}>{item.label.toUpperCase()}</Typography>
+        <Typography sx={{ fontSize: 25 }}>{item.data || "N/A"}</Typography>
+      </Grid>
+    );
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
       <Grid container alignItems="center" textAlign="center" rowGap={5} sx={{ mb: 5 }}>
-        {summaryData.map(
-          (stat, index): JSX.Element => (
-            <Grid key={index} item xs={4}>
-              {stat.teamCode && (
-                <FontAwesomeIcon
-                  icon={faTshirt}
-                  size="3x"
-                  color={getTeamShirtColour(stat.teamCode)}
-                />
-              )}
-              <Typography sx={{ fontSize: 25 }}>{stat.label.toUpperCase()}</Typography>
-              <Typography sx={{ fontSize: 25 }}>{stat.data}</Typography>
-            </Grid>
-          )
-        )}
+        {summaryData.map((stat, index): JSX.Element => renderGridItem(stat, index))}
       </Grid>
     </Box>
   );
