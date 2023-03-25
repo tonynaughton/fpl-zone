627661acc987d61fca02d820ae81c98120217458
TonyN96 <tonynaughton1@gmail.com>
2023-03-25 15:51:30 +0000

    Removed component label and fixed results flex props

--- src/components/layout/component_container.tsx
+++ src/components/layout/component_container.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 
 interface ComponentContainerProps {
   title: string;
@@ -10,42 +10,9 @@ interface ComponentContainerProps {
 }
 
 export const ComponentContainer = ({
-  title,
   flex,
   children
 }: ComponentContainerProps): JSX.Element => {
-  const theme = useTheme();
-
-  const ComponentLabel = (): JSX.Element => (
-    <Box
-      bgcolor={theme.palette.primary.main}
-      borderBottom='2px solid black'
-      borderRight='2px solid black'
-      color={theme.palette.primary.contrastText}
-      height='auto'
-      left={0}
-      padding={0.8}
-      position='absolute'
-      top={0}
-      width='auto'
-      zIndex='fab'
-    >
-      <Typography
-        className='text-ellipsis'
-        sx={{
-          [theme.breakpoints.down("md")]: {
-            fontSize: "1rem"
-          },
-          [theme.breakpoints.up("md")]: {
-            fontSize: "1.2rem"
-          }
-        }}
-        width='100%'
-      >
-        {title.toUpperCase()}
-      </Typography>
-    </Box>
-  );
 
   return (
     <Box
@@ -60,7 +27,6 @@ export const ComponentContainer = ({
       position='relative'
       width='100%'
     >
-      <ComponentLabel />
       {children}
     </Box>
   );
--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -11,7 +11,7 @@ import FixtureDetailsModal from "./match_details_modal/match_details_modal";
 import { ResultContainer } from "./result_container";
 
 export default function Results(): JSX.Element {
-  const { gameweeks, fixtures, isMobile } = useContext(AppDataContext) as AppData;
+  const { gameweeks, fixtures } = useContext(AppDataContext) as AppData;
 
   const currentGameweek = gameweeks.find((gw) => gw.is_current);
 
@@ -39,7 +39,7 @@ export default function Results(): JSX.Element {
 
   const GameweekNavigator = (): JSX.Element => {
     return (
-      <Box className='flex-center' overflow='hidden' width='100%'>
+      <Box className='flex-center' pt={1} width='100%'>
         <IconButton
           data-testid='prev-gameweek-btn'
           disabled={selectedGameweek <= 1}
@@ -67,44 +67,32 @@ export default function Results(): JSX.Element {
     );
   };
 
-  const GameweekFixtures = (): JSX.Element => {
-    if (isEmpty(gameweekFixtures)) {
-      return <Notifier message='No fixtures' type='warning' />;
-    }
-
-    return (
-      <Box
-        display='flex'
-        flexDirection='column'
-        height='100%'
-        justifyContent='space-evenly'
-        sx={{
-          overflowX: "hidden",
-          overflowY: "auto"
-        }}
-        width='100%'
-      >
-        {gameweekFixtures.map((fixture, key) => (
-          <ResultContainer
-            fixture={fixture}
-            key={key}
-            onFixtureClick={handleFixtureClick}
-          />
-        ))}
-      </Box>
-    );
-  };
+  const GameweekFixtures = (): JSX.Element => (
+    <Box
+      display='flex'
+      flexDirection='column'
+      flexGrow={1}
+      overflow='scroll'
+    >
+      {
+        isEmpty(gameweekFixtures)
+          ? <Notifier message='No fixtures' type='warning' />
+          : gameweekFixtures.map((fixture, key) => (
+            <ResultContainer
+              fixture={fixture}
+              key={key}
+              onFixtureClick={handleFixtureClick}
+            />
+          ))
+      }
+    </Box>
+  );
 
   return (
     <Box
-      alignItems='center'
       display='flex'
       flexDirection='column'
       height='100%'
-      justifyContent='center'
-      overflow='hidden'
-      pt={isMobile ? 3 : 6}
-      width='100%'
     >
       <GameweekNavigator />
       <GameweekFixtures />
